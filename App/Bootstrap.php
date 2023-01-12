7c9d06cdf1623779e0103b6bae209d9eab782ddf
Stevie <791369498@qq.com>
2023-01-12 10:20:32 +0800

    update

--- App/Bootstrap.php
+++ App/Bootstrap.php
@@ -1,10 +1,15 @@
 <?php declare(strict_types=1);
-
 namespace App;
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ERROR);
 
 class Bootstrap
 {
     public function __construct(){
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Allow-Origin: *');
         Route::start();
     }
 
--- index.php
+++ index.php
@@ -1,12 +1,5 @@
-<?php declare(strict_types=1);
-
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ERROR);
+<?php
 
 require './vendor/autoload.php';
-
-header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Origin: *');
 
 \App\Bootstrap::start();
